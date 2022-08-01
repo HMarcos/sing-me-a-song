445dab860951f1a31f9e06d927ae22cc89dd42c4
Marcos Henrique <marcosmarcone48@gmail.com>
2022-08-01 15:37:40 -0300

    TEST: implement e2e to upvote and downvote recommendations

--- front-end/cypress/e2e/app.cy.js
+++ front-end/cypress/e2e/app.cy.js
@@ -46,6 +46,47 @@ describe('Create a recommendation test suite', () => {
   });
 });
 
+describe('Up vote recommendation test suit', () => {
+  it('should increment by 1 the score when upvote a recommendation', () => {
+    const recommendation = {
+      name: faker.music.songName(),
+      youtubeLink: 'https://www.youtube.com/watch?v=e6QhH3q-UJE',
+    };
+
+    cy.createRecommendation(recommendation);
+    cy.get('#upvote').click();
+    cy.get('.score').should('contain.text', '1');
+  });
+});
+
+describe('Down vote recommendation test suit', () => {
+  it('should decrement by 1 the score when downvote a recommendation', () => {
+    const recommendation = {
+      name: faker.music.songName(),
+      youtubeLink: 'https://www.youtube.com/watch?v=e6QhH3q-UJE',
+    };
+
+    cy.createRecommendation(recommendation);
+    cy.get('#downvote').click();
+    cy.get('.score').should('contain.text', '-1');
+  });
+
+  it('should decrement the score by 1 until -6 and exclude the recommendation', () => {
+    const recommendation = {
+      name: faker.music.songName(),
+      youtubeLink: 'https://www.youtube.com/watch?v=e6QhH3q-UJE',
+    };
+
+    cy.createRecommendation(recommendation);
+    cy.get('#downvote').click().click().click().click().click().click();
+
+    cy.contains(`${recommendation.name}`).should('not.exist');
+    cy.contains(`No recommendations yet! Create your own :)`).should(
+      'be.visible'
+    );
+  });
+});
+
 afterEach(() => {
   cy.resetRecommendations();
 });
